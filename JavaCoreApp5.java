6fb4f65ee0f831c4249c915d49fa90e933cb0124
Dihus28 <gpeschannyi@mail.ru>
2021-11-13 21:43:02 +0300

    JavaCore5

---
+++ JavaCoreApp5.java
@@ -0,0 +1,15 @@
+package JavaCore5;
+
+import java.util.Arrays;
+
+public class JavaCoreApp5 {
+    public static void main(String[] args) {
+            AppData data = new AppData();
+            data.readFromFile("example.csv");
+            System.out.println(Arrays.toString(data.getHeader()));
+            System.out.println(Arrays.deepToString(data.getData()));
+
+            data.writeToCSV("new_example.csv");
+        }
+
+    }
