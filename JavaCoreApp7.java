ec2469e2cae636d8e0d47bb0149f54c4ac871512
Dihus28 <gpeschannyi@mail.ru>
2021-11-17 20:31:04 +0300

    JavaCore7

---
+++ JavaCoreApp7.java
@@ -0,0 +1,9 @@
+package JavaCore7;
+
+public class JavaCoreApp7 {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
+    }
+}
---
+++ Periods.java
@@ -0,0 +1,7 @@
+package JavaCore7;
+
+public enum Periods {
+    NOW,
+    FIVE_DAYS,
+    CUSTOM
+}
